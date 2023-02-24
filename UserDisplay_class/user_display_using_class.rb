f2577cbf2c1d170cbef06b098cdad43c0917c4c1
aki <akihiro.okuda366@gmail.com>
2023-02-24 14:51:16 +0900

    クラスを使った実装

--- UserDisplay_class/user_display_using_class.rb
+++ UserDisplay_class/user_display_using_class.rb
@@ -1,14 +1,25 @@
+# Userクラスを定義する
+class User
+  attr_reader :first_name, :last_name, :age
+
+  def initialize(first_name, last_name, age)
+    @first_name = first_name
+    @last_name = last_name
+    @age = age
+  end
+end
+
 # ユーザのデータを作成する
 user = []
-user << { first_name: 'Alice', last_name: 'Ruby', age: 20 }
-user << { first_name: 'Bob', last_name: 'Pytho', age: 30 }
+user << User.new('Alice', 'Ruby', 20)
+user << User.new('Bob', 'Pytho', 30)
 
-# 指名を作成するメソッド
+# 氏名を作成するメソッド
 def full_name(user)
-  "#{user[:first_name]} #{user[:last_name]}"
+  "#{user.first_name} #{user.last_name}"
 end
 
 # ユーザのデータを表示する
 user.each do |user|
-  puts "氏名: #{full_name(user)}, 年齢: #{user[:age]}"
+  puts "氏名: #{full_name(user)}, 年齢: #{user.age}"
 end
