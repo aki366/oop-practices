f2b1ba5e871fd4eb0a41131b1de6955624a0fb12
aki <akihiro.okuda366@gmail.com>
2023-02-24 14:48:20 +0900

    コードの移し替え

---
+++ UserDisplay_class/user_display_without_classes.rb
@@ -0,0 +1,14 @@
+# ユーザのデータを作成する
+user = []
+user << { first_name: 'Alice', last_name: 'Ruby', age: 20 }
+user << { first_name: 'Bob', last_name: 'Pytho', age: 30 }
+
+# 氏名を作成するメソッド
+def full_name(user)
+  "#{user[:first_name]} #{user[:last_name]}"
+end
+
+# ユーザのデータを表示する
+user.each do |user|
+  puts "氏名: #{full_name(user)}, 年齢: #{user[:age]}"
+end
